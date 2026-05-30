50454b3fa3aa763e45f769c6e1a319881dcbf442
DESKTOP-ETOMKT5\ghagh <timber.hearth.start@gmail.com>
2026-05-30 21:38:38 +0900

    비번 해쉬화 + 엑세스 토큰

--- src/core/security.py
+++ src/core/security.py
@@ -1,1 +1,40 @@
-﻿
+﻿import os
+from typing import Any
+
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone, UTC
+from jose import jwt, JWTError
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.environ.get("SECRET_KEY")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def GetPasswordHash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def VerifyPassword(password: str, _hash: str) -> bool:
+    return pwd_context.verify(password, _hash)
+
+
+def CreateAccessToken(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(UTC) + expires_delta
+    else:
+        expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def VerifyAccessToken(token: str) -> dict[str, Any] | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload["exp"] = datetime.fromtimestamp(payload["exp"], timezone.utc)
+        return payload
+    except JWTError:
+        return None
