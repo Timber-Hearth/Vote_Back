33f295ac85b2f9d9efbdfc1d15e0e29a3423968e
DESKTOP-ETOMKT5\ghagh <timber.hearth.start@gmail.com>
2026-05-31 08:40:11 +0900

    회원가입

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -1,1 +1,13 @@
-﻿
+﻿from pydantic import BaseModel
+
+class SignUpRequest(BaseModel):
+    login_id: str
+    password: str
+    
+class LoginRequest(BaseModel):
+    login_id: str
+    password: str
+    
+class TokenRequest(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,1 +1,21 @@
-﻿
+﻿from sqlalchemy.orm import Session
+
+from core.security import GetPasswordHash
+from models import User
+
+
+def SignUp(db: Session, login_id : str, password : str):
+    exist_user = (
+        db.query(User)
+        .filter(User.login_id == login_id)
+        .first()
+    )
+    if exist_user:
+        raise Exception("User already exists")
+    else:
+        hash_password = GetPasswordHash(password)
+        user = User(login_id=login_id, password_hash=hash_password)
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
