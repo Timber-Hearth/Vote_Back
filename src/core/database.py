d1180f130189a6ae2a78add37445e52e09813aef
DESKTOP-ETOMKT5\ghagh <timber.hearth.start@gmail.com>
2026-05-30 18:57:52 +0900

    db provider

--- src/core/database.py
+++ src/core/database.py
@@ -1,1 +1,28 @@
-﻿
+﻿import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext import declarative
+from sqlalchemy.future import engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+Base = declarative_base()
+
+def GetDB():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
