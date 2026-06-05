c1e889c942cf63c60865945da7a436ccc8fc236e
DESKTOP-ETOMKT5\ghagh <timber.hearth.start@gmail.com>
2026-06-05 19:10:57 +0900

    개발용 cors 나중에 수정할것

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 ﻿from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.v1.votes import vote_router
 from src.api.v1.auth import auth_router
@@ -8,6 +9,15 @@ app = FastAPI()
 app.include_router(router=auth_router, prefix="/auth", tags=["auth"])
 app.include_router(router=poll_router, prefix="/polls", tags=["polls"])
 app.include_router(router=vote_router, prefix="/vote", tags=["vote"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
